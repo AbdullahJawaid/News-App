599cecc78809692e7a727c3a78f3fa6b3b7939ac
AbdullahJawaid <91978361+AbdullahJawaid@users.noreply.github.com>
2023-11-13 22:09:53 +0500

    Initial commit

---
+++ newsapp/src/components/News.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import NewsItem from './NewsItem'
+
+export default function News() {
+  return (
+    <div>
+        <NewsItem cardTitle='NewsApp' />
+      <h1>This is new components</h1>
+    </div>
+  )
+}

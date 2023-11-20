c6f3228e516f060f2d5115e14f7ce00b280864e4
AbdullahJawaid <91978361+AbdullahJawaid@users.noreply.github.com>
2023-11-21 04:21:33 +0500

    first commit

--- newsapp/src/components/Navbar.js
+++ newsapp/src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
     <div>
       <nav className="navbar bg-dark navbar-expand-lg bg-danger-tertiary " data-bs-theme="dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">Navbar</a>
+    <a className="navbar-brand" href="/">NewsMonkey</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import blankNews from './blankNews.jpg'
 
 export default function NewsItem(props) {
+  let blankNewsImg=blankNews;
   return (
-    <div>
+    <div className='my-3'>
      <div className="card" style={{width: "18rem"}}>
-        <img src="..." className="card-img-top" alt="..."/>
+        <img src={props.imageUrl || blankNewsImg} className="card-img-top" alt="..."/>
         <div className="card-body">
-            <h5 className="card-title">{props.cardtitle}</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
+            <h5 className="card-title">{props.cardTitle}...</h5>
+            <p className="card-text">{props.cardDes}</p>
+            <a href={props.newsUrl} target='_blank' className="btn btn-sm btn-primary">Read More</a>
         </div>
      </div>
       
